4d99e3acc28eb77c9bbd7585b61fde68bdc24b03
HS831 <harrymishra831@gmail.com>
2022-04-12 11:23:34 +0530

    Added the code

---
+++ routes/viewsRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const viewsController = require('../controller/viewsController');
+const authController = require('../controller/authController');
+
+const router = express.Router();
+router.use(authController.isLoggedIn);
+
+router.get('/', viewsController.getFrontPage);
+router.get('/discover', viewsController.getOverview);
+router.get('/tour/:slug', viewsController.getTour);
+router.get('/login', viewsController.getloginForm);
+router.get('/signup', viewsController.getsignupForm);
+router.get('/create-review', viewsController.getreviewForm);
+router.get('/me', authController.protect, viewsController.myaccount);
+
+module.exports = router;
